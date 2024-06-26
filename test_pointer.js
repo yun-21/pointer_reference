488b3b0a1bc72adb20414e0d415096325c03a136
shinjiyun <ranji12@naver.com>
2024-06-26 11:43:48 +0900

    원시 값 작성, 위치 값

---
+++ test_pointer.js
@@ -0,0 +1,13 @@
+//a와 b는 원시값
+const a = 1;
+const b = 1;
+console.log(a === b)
+
+const c = [1]; //? 변수 c의 인덱스 0번째는 
+const d = [1];
+
+//신지윤 안의 구하림씨 , 김정수씨 안의 구하림씨
+console.log(c[0]===d[0])
+
+//신지윤과 김정수씨 
+console.log(c===d)
